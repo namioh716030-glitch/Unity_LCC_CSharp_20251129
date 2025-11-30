042311f12f13b0772a9a3119a398ecdb2cef2b98
namioh716030-glitch <namioh716030@gmail.com>
2025-11-30 22:37:02 +0800

    Update_課程作業練習_變數_浮點

---
+++ Assets/Scripts/Practise_01_Variables.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+///<summary>
+///課堂練習一：在「課程_1_變數」場景內練習
+///</summary>
+/*1.建立一個新的C#腳本，命名為「Practise_01_Variables」
+ *2.建立空物件「練習_1_變數」並套用
+*/
+// 宣告四個變數：
+/* 角色名稱 BlackMarketMerchants，預設值為布X前男友，公開
+ *角色等級 level，預設值為 5 ，私人 byte
+ *角色血量 hp，預設值為 100，公開 ushort
+ *販售中 hasSale，預設值為 true，私人
+ *跳躍高度 jumpHeight，預設值為 77.5，私人 float
+*/
+// 販售中物品資訊，預設值如下：
+// 1.價格 price，預設值為 999，公開 ushort
+/* 2.武器 weapon，預設值為往日之影，公開
+ * 武器傷害 weaponDamage，預設值為 -30000，私人 ushort
+ * 武器攻速 weaponSpeed，預設值為 3.5，私人 float
+ * 武器揮動扣除HP weaponSwingHpCost，預設值為 50，私人 byte
+*/
+//角色屬性資訊
+// 1.角色名稱 playerName，預設值為「布X現男友」，公開
+// 2.角色等級 playerLevel，預設值為 30，私人 byte
+// 3.角色hp playerHp，預設值為 1500，公開 ushort
+// 4.角色所持有金幣 playerCoin，預設值為 1000，私人 ushort
+//角色特殊能力
+// 4.是否獲得龍魂 hasDragonSoul，預設值為 true，私人
+
+public class Practise_01_Variables : MonoBehaviour
+{
+    //商人角色資訊
+    public string merchantName = "布X前男友";
+    public byte merchantLevel = 5;
+    public ushort merchantHp = 100;
+    private float merchantJumpHeight = 77.5f;
+    private float merchantMoveSpeed = 66.5f;
+    private bool merchantHasSale = true;
+    //販售中物品資訊
+    public ushort itemPrice = 999;
+    public string itemWeaponName = "往日之影";
+    private short itemWeaponDamage = -30000;
+    private float itemWeaponSpeed = 3.5f;
+    private byte itemWeaponSwingHpCost = 50;
+    //玩家角色屬性資訊
+    public string playerName = "布X現男友";
+    public byte playerLevel = 30;
+    public ushort playerHp = 1500;
+    private float playerJumpHeight = 38.75f;
+    private float playerMoveSpeed = 33.25f;
+    public ushort playerCoin = 1000;
+    private bool hasDragonSoul = true;
+}
---
+++ Assets/Scripts/Practise_02_DataType.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 課程練習二：在「課程_2_資料型態」場景內練習
+/// </summary>
+//新增一個C#腳本，命名為「Practise_02_DataType」
+//建立空物件「練習_2_資料型態」並套用
+/*宣告三個公開的變數：
+ * 1.使用浮點數 float，預設值為 7.7f 
+ * 2.使用倍精度浮點數 double，預設值為 0.12345
+ * 3.使用範圍最大的浮點數 decimal，預設值為 1.23456789m
+*/
+public class Practise_02_DataType : MonoBehaviour
+{
+    //風險骰子機制設定
+    private string itemName = "風險骰子";
+    // 骰到失敗機率，私人
+    private float failProbability = 75.75f; //失敗機率：0-100%
+    // 失敗懲罰成立，私人
+    private bool isPunishement = true;
+    // 使用浮點數 float，預設值為 7.7f
+    // 懲罰倍數
+    public float PunishementMultiplier = 7.7f; //根據點數乘以此倍數作為懲罰
+    // 使用倍精度浮點數 double，預設值為 0.12345f
+    // 骰到幸運機率
+    public double LuckyProbability = 0.12345; //幸運機率：0-100%
+    // 成功獎勵成立，私人
+    private bool isReward = true;
+    // 使用範圍最大的浮點數 decimal，預設值為 1.23456789f
+    // 骰到安全機率
+    // public decimal SafeProbability = 1.23456789m;
+    public double SafeProbability = 1.23456789; //安全機率：0-100%
+    // 骰到安全獎勵，免除懲罰，預設非安全，私人
+    private bool isSafe = false;
+}
