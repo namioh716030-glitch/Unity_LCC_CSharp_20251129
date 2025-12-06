47ca3ea58cac4fd0863c2a147929554c681136e6
namioh716030-glitch <namioh716030@gmail.com>
2025-12-06 10:39:01 +0800

    Update_new_object

---
+++ Assets/Practise/Practise_04_Method.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 課程作業_04_方法練習
+/// </summary>
+/* 條件：
+ * 建立空物件「練習_4_方法」並套用
+ * 宣告兩個方法傳回布林值
+ * 方法內要包含一段 Debug.Log
+ * 一個傳回 true
+ * 一個傳回 flase
+*/
+public class Practise_04_Method : MonoBehaviour 
+{
+    
+}
---
+++ Assets/Practise/Prcatise_03_Operator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// 課程作業_03_運算子練習
+/// </summary>
+/* 條件：
+ * 建立空物件「練習_3_運算子」並套用
+ * 計算「6 / 2(1+2)
+*/
+public class Prcatise_03_Operator : MonoBehaviour
+{
+    private void Awake()
+    {
+        float numberA = 6;
+        float numberB = 2;
+        float numberC = (1 + 2);
+        float numberD = (numberA / numberB * numberC);
+
+        Debug.Log(numberD);
+    }
+}
