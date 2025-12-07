d2256e89d1445ad28fe6c401b2ae74691dbcc223
namioh716030-glitch <namioh716030@gmail.com>
2025-12-07 15:08:49 +0800

    Update_Class_05_Selection

---
+++ Assets/Scripts/Class_05_1_Selection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// 命名空間：
+// 將類別分類，類似倉庫的概念，不同倉庫可以有相同名稱類別
+// 語法： namespace 命名空間名稱 { 內容 }
+namespace Lee
+{
+    /// <summary>
+    /// 選取陳述式 Selection Statement
+    /// 1. if 判斷式
+    /// 2. Switch 判斷式
+    /// </summary>
+    public class Class_05_1_Selection : MonoBehaviour
+    {
+
+    }
+}
+
